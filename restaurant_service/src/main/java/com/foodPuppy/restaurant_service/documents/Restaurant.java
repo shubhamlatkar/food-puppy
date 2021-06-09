a68e59d635fb09c962c655a597a63940d2ebea47
shubhamlatkar <shubhamlatkar@gmail.com>
2021-06-09 18:30:53 +0530

    removed lombok

--- restaurant_service/src/main/java/com/foodPuppy/restaurant_service/documents/Restaurant.java
+++ restaurant_service/src/main/java/com/foodPuppy/restaurant_service/documents/Restaurant.java
@@ -1,8 +1,5 @@
 package com.foodPuppy.restaurant_service.documents;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,9 +7,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Document
 public class Restaurant {
 
@@ -25,5 +19,37 @@ public class Restaurant {
 
     public Restaurant(String restaurant_name) {
         this.restaurant_name = restaurant_name;
+    }
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String id, String restaurant_name) {
+        this.id = id;
+        this.restaurant_name = restaurant_name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getRestaurant_name() {
+        return restaurant_name;
+    }
+
+    public void setRestaurant_name(String restaurant_name) {
+        this.restaurant_name = restaurant_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id='" + id + '\'' +
+                ", restaurant_name='" + restaurant_name + '\'' +
+                '}';
     }
 }
