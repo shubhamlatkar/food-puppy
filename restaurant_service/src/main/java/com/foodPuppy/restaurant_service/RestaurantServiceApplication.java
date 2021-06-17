5c620b81b9dff6fe72bcaf565f79525464ab931e
shubhamlatkar <shubhamlatkar@gmail.com>
2021-06-17 12:05:55 +0530

    Temp changes in api gateway cross origin

--- restaurant_service/src/main/java/com/foodPuppy/restaurant_service/RestaurantServiceApplication.java
+++ restaurant_service/src/main/java/com/foodPuppy/restaurant_service/RestaurantServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@CrossOrigin("*")
 public class RestaurantServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestaurantServiceApplication.class, args);
