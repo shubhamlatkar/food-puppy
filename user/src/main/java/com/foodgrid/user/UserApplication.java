d44841e834c1476b5f8837549236eacea76bb964
shubhamlatkar <shubhamlatkar@gmail.com>
2021-06-20 10:19:38 +0530

    Added scan for base pkg

--- user/src/main/java/com/foodgrid/user/UserApplication.java
+++ user/src/main/java/com/foodgrid/user/UserApplication.java
@@ -6,7 +6,10 @@ import com.foodgrid.common.utils.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableEurekaClient
 @CrossOrigin("*")
+@ComponentScan("com.foodgrid")
+@EnableMongoRepositories("com.foodgrid")
+@EntityScan("com.foodgrid")
 public class UserApplication {
 
     public static void main(String[] args) {
