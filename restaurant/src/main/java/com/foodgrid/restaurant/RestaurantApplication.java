f7c9a3bb26d6c0443f08a372fbe22f3690287560
shubhamlatkar <shubhamlatkar@gmail.com>
2021-06-20 10:08:38 +0530

    Removed not required config

--- restaurant/src/main/java/com/foodgrid/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/com/foodgrid/restaurant/RestaurantApplication.java
@@ -6,10 +6,7 @@ import com.foodgrid.common.security.SecurityEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,9 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableEurekaClient
 @CrossOrigin("*")
-@ComponentScan("com.foodgrid")
-@EnableMongoRepositories("com.foodgrid")
-@EntityScan("com.foodgrid")
 public class RestaurantApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestaurantApplication.class, args);
