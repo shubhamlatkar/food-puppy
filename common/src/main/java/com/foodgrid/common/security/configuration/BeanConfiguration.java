06db25c6ff392140caea6356782c4936d2dbf149
shubhamlatkar <shubhamlatkar@gmail.com>
2021-06-28 23:25:05 +0530

    cors bean changes

--- common/src/main/java/com/foodgrid/common/security/configuration/BeanConfiguration.java
+++ common/src/main/java/com/foodgrid/common/security/configuration/BeanConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BeanConfiguration {
@@ -16,6 +18,16 @@ public class BeanConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 
 }
