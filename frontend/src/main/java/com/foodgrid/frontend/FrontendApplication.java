55a01f1f62c58e61de52b7ad1fc60ef5f915fc66
shubhamlatkar <shubhamlatkar@gmail.com>
2021-07-13 14:13:11 +0530

    added back eureka for FrontendApplication

--- frontend/src/main/java/com/foodgrid/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/foodgrid/frontend/FrontendApplication.java
@@ -3,8 +3,10 @@ package com.foodgrid.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FrontendApplication {
     public static void main(String[] args) {
         SpringApplication.run(FrontendApplication.class, args);
