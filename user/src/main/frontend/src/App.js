ba2cfa3f5b588d0d08fb248d616fbfbd28be2ed0
shubhamlatkar <shubhamlatkar@gmail.com>
2021-07-21 22:31:27 +0530

    added custom banner and temporarily changed UI

--- user/src/main/frontend/src/App.js
+++ user/src/main/frontend/src/App.js
@@ -1,29 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   NavLink,
   Route,
   Switch
 } from "react-router-dom";
-import "./styles.css";
+import "./styles.scss";
 import Banner from "./components/Banner";
 import Dashboard from "./components/Dashboard";
+import axios from "axios";
+import Loader from "./components/Loader";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState({});
+  const [notification, setNotification] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+        url: "/user/api/v1/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        data: JSON.stringify({ username: "testUser", password: "test" })
+    })
+      .then((res) => {
+            console.log("success", res.data);
+           setUser({ ...res.data });
+           let source = null;
+           source = new EventSource(
+             "/notification/api/v1/notification/user/" + res.data.id
+           );
+           source.addEventListener("notification", function (event) {
+             var data = event.data;
+             console.log("Event data : ", data);
+             setNotification([...notification, data]);
+           });
+           source.onerror = (err) => console.log("Error", err);
+           setLoading(false);
+      })
+      .catch((err) => setLoading(false));
+  }, []);
+
   return (
     <div className="App App-header">
-      <Router>
-        <NavLink to="/user/member/dashboard">Dashboard</NavLink>
-        <NavLink to="/user/member/banner">Banner</NavLink>
-        <h1>Test frontend for foodgrid user</h1>
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/user/member/dashboard" component={Dashboard} />
-          <Route path="/user/member/banner" component={Banner} />
-        </Switch>
-      </Router>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Router>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/banner">Banner</NavLink>
+          <h2>Test frontend for foodgrid user</h2>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              component={() => (
+                <Dashboard user={user} notification={notification} />
+              )}
+            />
+            <Route
+              path="/dashboard"
+              component={() => (
+                <Dashboard user={user} notification={notification} />
+              )}
+            />
+            <Route path="/banner" component={Banner} />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
-}
+};
 
 export default App;
