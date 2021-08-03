594c50444da227a11e15d7d0f8b6766982503007
shubhamlatkar <shubhamlatkar@gmail.com>
2021-08-04 00:19:13 +0530

    removed duplicate code and added code-ql config

--- common/src/main/java/com/foodgrid/common/payload/dto/response/JwtResponse.java
+++ common/src/main/java/com/foodgrid/common/payload/dto/response/JwtResponse.java
@@ -1,51 +1,15 @@
 package com.foodgrid.common.payload.dto.response;
 
-public class JwtResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
     private String token;
     private String username;
     private String id;
-
-    public JwtResponse() {
-    }
-
-    public JwtResponse(String token, String username, String id) {
-        this.token = token;
-        this.username = username;
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "JwtResponse{" +
-                "token='" + token + '\'' +
-                ", username='" + username + '\'' +
-                ", id='" + id + '\'' +
-                '}';
-    }
 }
 
