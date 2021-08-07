28c6f83bd5f3220ce2290b099c063a68fd70f5ba
shubhamlatkar <shubhamlatkar@gmail.com>
2021-08-07 10:39:24 +0530

    adding tests and testing common pkg

--- common/src/test/java/com/foodgrid/common/unit/AuthenticationEventHandlerImplTests.java
+++ common/src/test/java/com/foodgrid/common/unit/AuthenticationEventHandlerImplTests.java
@@ -1,15 +1,32 @@
 package com.foodgrid.common.unit;
 
+import com.foodgrid.common.event.outbound.AuthenticationEvent;
 import com.foodgrid.common.event.service.AuthenticationEventHandlerImplementation;
+import com.foodgrid.common.payload.dto.event.UserAuthEventDTO;
 import com.foodgrid.common.security.implementation.UserDetailsServiceImplementation;
+import com.foodgrid.common.security.model.aggregate.Authority;
+import com.foodgrid.common.security.model.aggregate.Role;
+import com.foodgrid.common.security.model.aggregate.User;
+import com.foodgrid.common.security.model.entity.UserMetadata;
+import com.foodgrid.common.security.repository.RoleRepository;
 import com.foodgrid.common.security.repository.UserRepository;
+import com.foodgrid.common.utility.UserActivities;
+import com.foodgrid.common.utility.UserTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @DataMongoTest
 class AuthenticationEventHandlerImplTests {
@@ -23,11 +40,24 @@ class AuthenticationEventHandlerImplTests {
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private RoleRepository roleRepository;
+
     @Test
     void contextLoads() {
-        String expect = "test";
-        String actual = "test";
-        Assertions.assertEquals(expect, actual);
+        var tempRole = new Role("USER", List.of(new Authority("1", "TEST_AUTH")));
+        when(roleRepository.findByName("USER")).thenReturn(java.util.Optional.of(tempRole));
+
+        var tempUser = new User("test_username", "1234567890", "testemail@email.com", "test_pass", List.of(tempRole), UserTypes.USER);
+        tempUser.setMetadata(new UserMetadata(new Date(), new Date(), UserActivities.LOGIN));
+        tempUser.setId("1");
+        when(userRepository.findByUsername("test_username"))
+                .thenReturn(java.util.Optional.of(tempUser));
+
+        var eventUser = new UserAuthEventDTO(UserTypes.USER, "1", "test_username", "test_pass", UserActivities.LOGIN, "test_token", "USER", "1234567890", "testemail@email.com");
+        var event = new AuthenticationEvent(true, List.of(eventUser));
+        authenticationEventHandlerImplementation.authConsumer(event);
+        Assertions.assertNotNull(roleRepository.findByName("USER"));
     }
 
 }
