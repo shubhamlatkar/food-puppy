5042be065fe1e092d29e2c0c604e6752cdb2ead1
shubhamlatkar <shubhamlatkar@gmail.com>
2021-08-09 21:30:51 +0530

    copying common

---
+++ common/src/test/java/com/foodgrid/common/unit/BeanConfigurationTests.java
@@ -0,0 +1,32 @@
+package com.foodgrid.common.unit;
+
+import com.foodgrid.common.security.configuration.BeanConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class BeanConfigurationTests {
+
+    @Autowired
+    private BeanConfiguration beanConfiguration;
+
+    @Test
+    void testBeanConfigurationTestsPasswordEncoder() {
+        Assertions.assertNotNull(beanConfiguration.passwordEncoder());
+    }
+
+    @Test
+    void testBeanConfigurationTestsInternalRestTemplate() {
+        Assertions.assertNotNull(beanConfiguration.internalRestTemplate());
+    }
+
+    @Test
+    void testBeanConfigurationTestsExternalRestTemplate() {
+        Assertions.assertNotNull(beanConfiguration.externalRestTemplate());
+    }
+
+}
