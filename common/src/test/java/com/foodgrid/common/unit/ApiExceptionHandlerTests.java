3eebe5e60a8456dea4a6c8edb0414db3e906b8ff
shubhamlatkar <shubhamlatkar@gmail.com>
2021-08-15 11:41:05 +0530

    Adding exception handling for controller advice

---
+++ common/src/test/java/com/foodgrid/common/unit/ApiExceptionHandlerTests.java
@@ -0,0 +1,58 @@
+package com.foodgrid.common.unit;
+
+
+import com.foodgrid.common.exception.exceptions.InternalServerErrorException;
+import com.foodgrid.common.exception.exceptions.InvalidDataException;
+import com.foodgrid.common.exception.exceptions.NotFoundException;
+import com.foodgrid.common.exception.handler.ApiExceptionHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class ApiExceptionHandlerTests {
+    @Test
+    void testApiExceptionHandlerTestsHandleInvalidDataException() {
+        var controllerAdvice = new ApiExceptionHandler();
+        try {
+            Assertions.assertNotNull(controllerAdvice.handleInvalidDataException(new InvalidDataException("Invalid inputs")));
+        } catch (InvalidDataException e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+    @Test
+    void testApiExceptionHandlerTestsHandleInternalErrorException() {
+        var controllerAdvice = new ApiExceptionHandler();
+        try {
+            Assertions.assertNotNull(controllerAdvice.handleInternalErrorException(new InternalServerErrorException("Internal server error", new Throwable())));
+        } catch (InvalidDataException e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+    @Test
+    void testApiExceptionHandlerTestsHandleMappingNotFoundException() {
+        var controllerAdvice = new ApiExceptionHandler();
+        try {
+            Assertions.assertNotNull(controllerAdvice.handleMappingNotFoundException(new NoHandlerFoundException("GET", "/test", new HttpHeaders())));
+        } catch (InvalidDataException e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+    @Test
+    void testApiExceptionHandlerTestsDataNotFoundException() {
+        var controllerAdvice = new ApiExceptionHandler();
+        try {
+            Assertions.assertNotNull(controllerAdvice.dataNotFoundException(new NotFoundException("Data not found")));
+        } catch (InvalidDataException e) {
+            Assertions.assertNotNull(e);
+        }
+    }
+
+}
