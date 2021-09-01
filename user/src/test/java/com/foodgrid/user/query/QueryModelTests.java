6b4a5be7fe0fc3e7eb0a897d147bdb1eaa23753a
shubhamlatkar <shubhamlatkar@gmail.com>
2021-09-01 17:44:48 +0530

    adding user module testcases

---
+++ user/src/test/java/com/foodgrid/user/query/QueryModelTests.java
@@ -0,0 +1,31 @@
+package com.foodgrid.user.query;
+
+
+import com.foodgrid.user.command.internal.model.entity.ItemCommandModel;
+import com.foodgrid.user.query.internal.model.aggregate.CartQueryModel;
+import com.foodgrid.user.query.internal.model.entity.ItemQueryModel;
+import com.foodgrid.user.shared.payload.CartEventDTO;
+import com.foodgrid.user.shared.utility.CartActivities;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class QueryModelTests {
+    @Test
+    @DisplayName("Tests CartQueryModel ")
+    void cartQueryModel() {
+        var cart = new CartQueryModel("1", "1", Set.of(new ItemQueryModel("1", "test", 12.23, 1)));
+        var cart2 = new CartQueryModel("2", "2");
+        Assertions.assertNotNull(cart2.addItem(new CartEventDTO(CartActivities.ADD_ITEM, new ItemCommandModel("1", "test", 12.23, 1), "1", "1")));
+        Assertions.assertNotNull(cart2.removeItem("1"));
+        Assertions.assertNotNull(cart2.increaseQuantity("1"));
+        Assertions.assertNotNull(cart2.decreaseQuantity("1"));
+        Assertions.assertNotNull(cart.getItems());
+    }
+}
---
+++ user/src/test/java/com/foodgrid/user/query/ResponsesTests.java
@@ -0,0 +1,28 @@
+package com.foodgrid.user.query;
+
+import com.foodgrid.user.query.internal.payload.response.AddressNotFoundResponse;
+import com.foodgrid.user.query.internal.payload.response.FindByAddressId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class ResponsesTests {
+    @Test
+    @DisplayName("Tests AddressNotFoundResponse ")
+    void addressNotFoundResponse() {
+        var addressNotFound = new AddressNotFoundResponse("1", "test");
+        Assertions.assertNotNull(addressNotFound.getMsg());
+    }
+
+    @Test
+    @DisplayName("Tests FindByAddressId ")
+    void findByAddressId() {
+        var findByAddressId = new FindByAddressId("1", "123");
+        Assertions.assertNotNull(findByAddressId.getAddressId());
+    }
+
+}
